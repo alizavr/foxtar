d9615e15249337b661c02498994116256de9989a
alizavr <alina.dmitrievna2407@gmail.com>
2021-02-21 20:37:03 +0200

    added footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,22 @@
+$('#carousel-products').owlCarousel({
+    loop:true,
+   nav: false,
+   dots: true,
+
+    responsive:{
+        0:{
+            items:2
+        },
+
+        768:{
+            items:4
+        }
+    }
+});
+
+
 $( ".filter__item" ).on( "click", function() {
     $(this).toggleClass('actived');
 });
 
-    $('#carousel-products').owlCarousel({
-        loop:true,
-       nav: false,
-       dots: true,
-  
-        responsive:{
-            0:{
-                items:2
-            },
-  
-            768:{
-                items:4
-            }
-        }
-    });
+   
